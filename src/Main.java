e7b0f7cfc13e3843ff98f4cacf5ee47afe60e4b3
mridul9-git <mridulthakur96@gmail.com>
2026-03-24 10:19:09 +0530

    uc2

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.LinkedHashSet;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
 
 //===== UC1 =====
@@ -15,5 +15,20 @@ public class Main {
         List<String> trainConsist = new ArrayList<>();
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + trainConsist.size());
-    }
-}
+
+//===== UC2 =====
+        List<String> passengerBogies = new ArrayList<>();
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+        System.out.println("\nPassenger Bogies after addition:");
+        System.out.println(passengerBogies);
+        passengerBogies.remove("AC Chair");
+        System.out.println("\nAfter removing AC Chair:");
+        System.out.println(passengerBogies);
+        if (passengerBogies.contains("Sleeper")) {
+            System.out.println("\nSleeper bogie exists in the train.");
+        }
+        System.out.println("\nFinal Passenger Bogie List:");
+        System.out.println(passengerBogies);
+    }}

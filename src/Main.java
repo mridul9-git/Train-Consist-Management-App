a95578ef2ac7bf009d96e8543e2a4c649afab828
mridul9-git <mridulthakur96@gmail.com>
2026-03-24 10:16:41 +0530

    uc1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+//===== UC1 =====
+        System.out.println("=== Train Consist Management App ===");
+        List<String> trainConsist = new ArrayList<>();
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial bogie count: " + trainConsist.size());
+    }
+}

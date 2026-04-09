c9bc597279dd7fd97d8c2892da4b538123c08986
mridulthakur96@gmail.com <grmithran@gmail.com>
2026-04-09 09:09:06 +0530

    UC17

--- src/Main.java
+++ src/Main.java
@@ -401,6 +401,21 @@ public class Main {
             System.out.print(c + " ");
         }
         System.out.println();
+//===== UC17 =====
+        System.out.println("\n=== UC17: Sort Bogie Names Using Arrays.sort() ===");
 
+
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
+
+
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
+
+
+        Arrays.sort(bogieNames);
+
+
+        System.out.println("Sorted Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
